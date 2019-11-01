b22e4da75f015f651c1dd76953d688a7bbf29828
DavidDiGioia <davidofjoy@gmail.com>
2019-11-01 17:31:44 -0400

    finished python script!!!

--- DataStructuresAndAlgorithms/src/algorithms/solve_linear_systems.py
+++ DataStructuresAndAlgorithms/src/algorithms/solve_linear_systems.py
@@ -1,6 +1,8 @@
 from sympy import *
-from sympy import linsolve, symbols, fraction
+from sympy import linsolve, symbols, fraction, Rational
 from math import factorial
+
+valid_solutions = []
 
 def get_coefficients(x, count):
   arr = []
@@ -9,31 +11,25 @@ def get_coefficients(x, count):
   return arr
 
 def get_equations_and_syms(b, sub_list, degree):
+  global basic_solve
   eqns = []
-  syms = symbols('x0:' + str(degree + 1))
+  syms = symbols('x' + str(len(sub_list)) + ':' + str(degree + 1))
   for i in range(0, len(b)):
     # -1 signifies a member of b to omit
     if b[i] == -1:
       continue
-    coefficients = get_coefficients(i+1, len(syms))
+    coefficients = get_coefficients(i+1, degree + 1)
     eqn = None
-    j = 0
-    for sym in syms:
+    denom = 1 if basic_solve else factorial((degree / 2))**2
+    for j in range(0, degree + 1):
       # substitute for known coefficients
-      s = sub_list[j] if j < len(sub_list) else sym
+      s = Rational(sub_list[j], denom) if j < len(sub_list) else syms[j - len(sub_list)]
       if eqn is None:
         eqn = coefficients[j] * s
       else:
         eqn += coefficients[j] * s
-      j += 1
     eqns.append(eqn - b[i])
   return eqns, syms
-
-def get_equations_omit(sub_first_n, omit, b, degree):
-  x = degree / 2
-  sub_list = [1, x, x*(x+1)*(4*x+5)/6]
-  print(sub_list)
-  return get_equations_and_syms(b, sub_list[:sub_first_n], degree)
 
 # does this solution have denominators of the form (n!)^2
 def expected_sol(sol_set, degree):
@@ -43,33 +39,75 @@ def expected_sol(sol_set, degree):
   # extract denominator from each coefficient in solution
   for sol in sol_set:
     for frac in sol:
-      if denom % fraction(frac)[1] != 0:
+      if frac < 0 or denom % fraction(frac)[1] != 0:
         return False
   return True
 
-def print_omitted_rec(vec, omitted_count, idx):
+def omit_combinations_rec(vec, omitted_count, idx, degree, sub_first_n):
+  global valid_solutions
+  global break_if_valid
+  global basic_solve
   if omitted_count == 0:
-    print(vec)
-    return
+    eqns, syms = get_equations_omit(sub_first_n, omitted_count, b, degree)
+    sol_set = linsolve(eqns, syms)
+    if basic_solve or expected_sol(sol_set, degree):
+      print("Found valid solution.")
+      valid_solutions += sol_set
+      if break_if_valid:
+        return True
+    else:
+      print("Found invalid solution.")
+    return False
   for i in range(idx, len(vec) - omitted_count + 1):
     temp = vec[i]
     vec[i] = -1
-    print_omitted_rec(vec, omitted_count - 1, i + 1)
+    if omit_combinations_rec(vec, omitted_count - 1, i + 1, degree, sub_first_n):
+      return True
     vec[i] = temp
+  return False
 
 
-def print_omitted(vec, omitted_count):
-  print_omitted_rec(vec, omitted_count, 0)
+def omit_combinations(vec, omitted_count, degree, sub_first_n):
+  omit_combinations_rec(vec, omitted_count, 0, degree, sub_first_n)
 
-degree = 12
-# -1 indicates an omitted element
-b = [4116, 14220, 41872, 115668, 306410, 783536, 1938602, 4645126, 10784280, 24267500, 52950744, 112087996, 230348214, 459963092, 893334830]
-b = [1, 2, 3, 4]
-print_omitted(b, 2)
 
-# eqns, syms = get_equations_omit(0, -1, b, degree)
-# sol_set = linsolve(eqns, syms)
+def get_equations_omit(sub_first_n, omit, b, degree):
+  x = degree / 2 - 1
+  sub_list = [1, x+1, x*(x+1)*(4*x+5)/6]
+  return get_equations_and_syms(b, sub_list[:sub_first_n], degree)
 
-# if expected_sol(sol_set, degree):
-#   print("Expected solution!")
-#   print(sol_set)
+# ---------------------------------------------------------------------
+# If you've already found solution, don't bother checking others
+break_if_valid = True
+# For solving numerator polynomials. omit_count and sub_first_n must be 0
+# to use basic solve!
+basic_solve = True
+# Degree of polynomial we're looking for
+degree = 5
+# How many data points to omit (every combination will be tried)
+omit_count = 0
+# Assume first n variables to be expected values (helpful if there are
+# not enough data points to find polynomial)
+# Currently must be <= 3
+sub_first_n = 0
+# Data points to fit polynomial to
+# -1 indicates an omitted element (shouldn't be entered manually, just use
+# omit_count and all combinations will be tried)
+b = [18, 129, 472, 1250, 2730, 5243]
+# ---------------------------------------------------------------------
+
+if len(b) >= (degree + 1) + omit_count - sub_first_n:
+  omit_combinations(b, omit_count, degree, sub_first_n)
+  print("Finished!\nFound " + str(len(valid_solutions)) + " valid solutions:")
+  denom = 1 if basic_solve else factorial((degree / 2))**2
+  for solution in valid_solutions:
+    for frac in solution:
+      if basic_solve:
+        print(frac)
+      else:
+        num = fraction(frac)[0] * (denom / fraction(frac)[1])
+        print(num, end='')
+        print('/' + str(denom))
+else:
+  print("Variables do not fulfill requirement:")
+  print("len(b) >= (degree + 1) + omit_count - sub_first_n")
